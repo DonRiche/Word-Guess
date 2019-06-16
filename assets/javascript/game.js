b69cf8bce51366469124bdfd73ada17bf3077c74
Ricardo Perdomo <hndgd1980@gmail.com>
2019-06-16 14:25:30 -0500

    added html

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+document.write()
+
+var listOfMovies = ["toy story,alladin,the little mermaid,boss baby,shrek,moana,zootopia,the lego movie,coco,finding nemo,how to train a dragon,frozen,up,the incredibles,lion king,bambi,dumbo"]
